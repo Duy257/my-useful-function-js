0379b385306676d267942c4e9025b97fb66260f6
Duy Nguyen <duyyb257946@gmail.com>
2024-01-13 14:40:36 +0700

    init

--- array/groupArray.js
+++ array/groupArray.js
@@ -18,3 +18,18 @@ function groupByValue(array, key) {
   map.forEach(value => result.push(value));
   return result;
 }
+
+// Example
+// const data = [
+//   { id: 1, category: 'A', value: 'apple' },
+//   { id: 2, category: 'B', value: 'banana' },
+//   { id: 3, category: 'A', value: 'apricot' },
+//   { id: 4, category: 'C', value: 'cherry' },
+//   { id: 5, category: 'B', value: 'blueberry' },
+//   { id: 6, category: 'A', value: 'avocado' },
+//   { id: 7, category: 'C', value: 'coconut' },
+//   { id: 7, category: 'D', value: 'hehe' },
+// ];
+
+// const result = groupByValue(data, 'category');
+// console.log(result);
